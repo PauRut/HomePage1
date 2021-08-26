9732342c8ff6aadfff82c9012a2f8afc685fb703
PauRut <52420196+PauRut@users.noreply.github.com>
2021-08-26 19:19:35 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+const firstName = 'Paulina';
+const age = 32;
+
+const humanOne = {
+    name: 'Maciek',
+    age: 25,
+    address: {
+        street: 'Warszawa',
+        city: 'Bialystok'
+    }
+}
+
+const humanTwo = {
+    name: 'Stefan',
+    age: 32,
+    address: {
+        street: humanOne.address.street,
+        city: 'Bialystok'
+    }
+}
+
+
+if (humanOne.age > humanTwo.age) {
+    console.log('Human One jest starczy');
+} else if (humanOne.age === humanTwo.age) {
+    console.log('Human Two i Human One lub sa rowiesnikami');
+} else {
+    console.log('Human Two jest starszy');
+}
+
+
+
+const myNumber = 8;
+
+switch (myNumber) {
+    case 7:
+        console.log('jestem 7');
+        break;
+    case 9:
+        console.log('jestem dziewiatka');
+        break;
+    default:
+        console.log('jestem czyms innym');
+
+
+
+
+}
+
+
+
+
+
+// function createContent(querySelectorContent, content) {
+//     const element = document.querySelector('.week-summary__description--js');
+//     element.innerHTML = `Nawet uzupelniam tresc javascriptem!`;
+// }
+
+// createContent('week-summary__description--js')
+
+// const mainHeader = document.querySelector ('.mainHeader');
+
+// console.log(mainHeader.innerHTML);
+
+
+// const button = document.querySelector('.header__button--js');
+// console.log(button)
+
+// button.addEventListener('click',(e) => {
+//     const header = document.querySelector ('.header__title--js');
+//     header.innerHTML='klik, klik';
+//     header.classList.toggle('header__title--red');
+// });/*
